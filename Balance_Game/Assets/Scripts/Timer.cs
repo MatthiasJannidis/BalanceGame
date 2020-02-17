2b2410b9a8be8443416a13b75dd5376bcdb18bd3
Clement-pirelli <clepirelli@gmail.com>
2020-02-17 11:12:58 +0100

    First Prototype : slots, blocks, game controller

---
+++ Balance_Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer
+{
+    float timer = .0f;
+    float passedTime = .0f;
+    public float PassedTime { get { return passedTime; } }
+    public bool isDone { get { return passedTime > timer; } }
+
+    public Timer(float timer) { this.timer = timer; }
+
+    public void tick() 
+    {
+        passedTime += Time.deltaTime;
+    }
+
+    public void reset(float newTimer) 
+    {
+        timer = newTimer;
+        passedTime = .0f;
+    }
+
+}
