f251422bb0f5e8c6cc8893977e485df98924489c
Clement-pirelli <clepirelli@gmail.com>
2020-02-21 14:26:13 +0100

    Flipped weak and strong sides, added sprites, added lines, added arrow indicator, minor other changes

---
+++ Balance_Game/Assets/Scripts/Line.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] GameSettings settings;
+    [SerializeField] int lineNumber;
+    [SerializeField] Color lineColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        float y = .0f;
+        switch (lineNumber) 
+        {
+            case 1:
+                y = settings.weakArrowFirstPointY;
+                break;
+            case 2:
+                y = settings.weakArrowSecondPointY;
+                break;
+            case 3:
+                y = settings.weakArrowThirdPointY;
+                break;
+            default:
+                y = settings.strongArrowGoalY;
+                break;
+        }
+
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.SetPosition(0, new Vector3(-10.0f, y, .0f));
+        lineRenderer.SetPosition(1, new Vector3(10.0f, y, .0f));
+        lineRenderer.startColor = lineColor;
+        lineRenderer.endColor = lineColor;
+    }
+}
---
+++ Balance_Game/Assets/Scripts/Lines.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lines : MonoBehaviour
+{
+    [SerializeField] Color lineColor;
+    [SerializeField] GameObject linePrefab;
+
+    [SerializeField] float startY;
+    [SerializeField] float endY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 10; i++) 
+        {
+            var currentLine = Instantiate(linePrefab);
+            var lineRenderer = currentLine.GetComponent<LineRenderer>();
+
+            float y = startY + (i/10.0f)*(endY-startY);
+
+
+            lineRenderer.useWorldSpace = true;
+            lineRenderer.SetPosition(0, new Vector3(-10.0f, y, .0f));
+            lineRenderer.SetPosition(1, new Vector3(10.0f, y, .0f));
+            lineRenderer.startColor = lineColor;
+            lineRenderer.endColor = lineColor;
+        }
+
+
+        
+    }
+}
