55fc0dc07e9642dbe6fd8407c0b81f15b1d859fb
Clement-pirelli <clepirelli@gmail.com>
2020-02-27 13:20:24 +0100

    Changed line positions

--- Balance_Game/Assets/Scripts/Lines.cs
+++ Balance_Game/Assets/Scripts/Lines.cs
@@ -7,7 +7,6 @@ public class Lines : MonoBehaviour
     [SerializeField] Color lineColor;
     [SerializeField] GameObject linePrefab;
 
-    [SerializeField] float startY;
     [SerializeField] float endY;
     [SerializeField] int lineNumber = 10;
 
@@ -15,12 +14,27 @@ public class Lines : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < lineNumber; i++) 
+        for(int i = 0; i < lineNumber/2; i++) 
         {
             var currentLine = Instantiate(linePrefab);
             var lineRenderer = currentLine.GetComponent<LineRenderer>();
 
-            float y = startY + (i/(float)lineNumber)*(endY-startY);
+            float y = ((i+1)/(float)(lineNumber/2.0f))*(endY);
+
+
+            lineRenderer.useWorldSpace = true;
+            lineRenderer.SetPosition(0, new Vector3(-7.0f, y, .0f));
+            lineRenderer.SetPosition(1, new Vector3(7.0f, y, .0f));
+            lineRenderer.startColor = lineColor;
+            lineRenderer.endColor = lineColor;
+        }
+
+        for (int i = 0; i < lineNumber/2; i++)
+        {
+            var currentLine = Instantiate(linePrefab);
+            var lineRenderer = currentLine.GetComponent<LineRenderer>();
+
+            float y = ((i + 1) / (float)(lineNumber/2.0f)) * (-endY);
 
 
             lineRenderer.useWorldSpace = true;
@@ -31,6 +45,5 @@ public class Lines : MonoBehaviour
         }
 
 
-        
     }
 }
